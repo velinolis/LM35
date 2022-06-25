4767ab24f3db436a0125c31ec1a07446ada5910c
velinolis <velinolis@gmail.com>
2022-06-25 11:12:27 +0200

    Update LM35.cpp

--- LM35/LM35.cpp
+++ LM35/LM35.cpp
@@ -1,39 +1,29 @@
 #include "LM35.h"
 #include "Arduino.h"
+#include "pin.h"
 
-LM35::LM35(int pinSensor) {
+LM35::LM35(int pinSensor, int pinNPN) {
   _pinSensor = pinSensor;
+  _pinNPN = pinNPN;
 }
 
 int LM35::readTemp() {
-  
-  /*
-  [(VCC*1000)/1024]/10 = 0.48828125
-  VCC = 5V
-  V to mV (*1000)
-  10mV = 1ºC
-*/
 
-  int Celsius = analogRead(_pinSensor) * 0.48828125;
-  //float Celsius = (analogRead(_pinSensor)*500)>>10;
+  /*
+    [(VCC*1000)/1024]/10 = 0.48828125
+    VCC = 5V
+    V to mV (*1000)
+    10mV = 1ºC
+  */
+
+  int Celsius = analogRead(_pinSensor) * 0.48828125; //*500)>>10;
   return Celsius;
 }
 
-void LM35::hotTemp(int temp) {
+void LM35::Thermostate(int temp) {
   if (readTemp() >= temp) {
-    Serial.println("HT");
+    digitalHigh(_pinNPN);
   } else {
-    Serial.println(readTemp());
+    digitalLow(_pinNPN);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
